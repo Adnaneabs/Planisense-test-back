c422f592f3e88747d59564162bf0e9759727631c
Adnaneabs <Elabbasadnane@gmail.com>
2022-12-30 14:37:47 +0100

    small change

--- src/test/java/com/test/test_planisense_back/TestTestPlanisenseBack.java
+++ src/test/java/com/test/test_planisense_back/TestTestPlanisenseBack.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestBackHibernateApplicationTests {
+class TestTestPlanisenseBack {
 
     @Test
     void contextLoads() {
